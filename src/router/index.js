c3dbe73ce8979c7e4a622d493e06d35fac03132d
man nguyen <nguyenlyminhman@gmail.com>
2020-10-21 11:17:34 +0700

    Edited Router

--- src/router/index.js
+++ src/router/index.js
@@ -12,21 +12,23 @@ import UseRefHook from '../module/use-ref';
 import UseImperativeHandleHook from '../module/use-imperative-handle';
 import UseLayoutEffectHook from '../module/use-layout-effect';
 import UseDebugValueHook from '../module/use-debug-value';
+import Contact from '../module/contact';
 
 function RouterHook() {
     return (
         <div>
             <Route exact path="/" component={Home} />
             <Route exact path="/use-state" component={UseStateHook} />
-            <Route exact path="/use-effect" component={UseEffectHook} /> 
+            <Route exact path="/use-effect" component={UseEffectHook} />
             <Route exact path="/use-context" component={UseContextHook} />
             <Route exact path="/use-reducer" component={UseReducerHook} />
             <Route exact path="/use-callback" component={UseCallbackHook} />
             <Route exact path="/use-memo" component={UseMemoHook} />
-            <Route exact path="/use-ref" component={UseRefHook} /> 
+            <Route exact path="/use-ref" component={UseRefHook} />
             <Route exact path="/use-imperative-handle" component={UseImperativeHandleHook} />
             <Route exact path="/use-layout-effect" component={UseLayoutEffectHook} />
             <Route exact path="/use-debug-value" component={UseDebugValueHook} />
+            <Route exact path="/contact" component={Contact} />
         </div>
     );
 }
