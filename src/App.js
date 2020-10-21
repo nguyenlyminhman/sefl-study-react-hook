92494a57ab8900a6b98c847ccc80d141d1665d1f
man nguyen <nguyenlyminhman@gmail.com>
2020-10-21 09:54:52 +0700

    Edited App.js file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import Header from './layout/header';
 import Navigator from './layout/navigator';
 import Footer from './layout/footer';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import RouterHook from './router';
 
 function App() {
   return (
     <Router>
       <Navigator />
-      <Header />
-      <div className='container'>
-
-      </div>
+      <RouterHook />
       <Footer />
     </Router>
   );
